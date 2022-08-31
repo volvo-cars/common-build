d0559e109cb1ac51abb93a3c34732ed362ecdb1c
Niclas Lindberg <nlindbe2@volvocars.com>
2022-08-31 08:59:09 +0200

    Using default docker-compose in docker dist
    Change-Id: Ifd3333ebcc997ec36636e0a63d86ee727ac8ce01

--- docker/dev/run.sh
+++ docker/dev/run.sh
@@ -3,6 +3,6 @@ export CURRENT_DIR=$(pwd)
 
 #docker run -v $CURRENT_DIR:/work -v $HOME/.ssh:/root_ssh -w /work -e USER -e VAULT_TOKEN -it onebuild-dev:latest /bin/bash #
 
-docker-compose -f $CURRENT_DIR/docker/dev/docker-compose.yml up --detach --force-recreate --remove-orphans 
+docker compose -f $CURRENT_DIR/docker/dev/docker-compose.yml up --detach --force-recreate --remove-orphans 
 echo Started containers. Use docker/dev/enter.sh container-name to enter a container
 docker ps
--- docker/dev/stop.sh
+++ docker/dev/stop.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 export CURRENT_DIR=$(pwd)
-docker-compose  -f $CURRENT_DIR/docker/dev/docker-compose.yml down
+docker compose  -f $CURRENT_DIR/docker/dev/docker-compose.yml down
